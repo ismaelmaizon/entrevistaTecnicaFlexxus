a4b996a2fcb4f4328eeb0a879680feeccafcf521
ismaelmaizon <ismael.maizon@gmail.com>
2025-05-27 15:23:21 -0300

    fourth commit

--- database/mysql.ts
+++ database/mysql.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env
+const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME } = process.env
 
 export const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
-  port: 3306, // Default MySQL port
+  port: DB_PORT,
   password: DB_PASSWORD,
   database: DB_NAME,
   waitForConnections: true,
